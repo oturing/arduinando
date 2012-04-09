1ddcdb1b13653989346d096c886807597375a273
Luciano Ramalho <luciano@ramalho.org>
2012-04-09 19:34:22 -0300

    simplificando codigo; corrigindo comentarios

--- Blink7SegFreqVar/Blink7SegFreqVar.ino
+++ Blink7SegFreqVar/Blink7SegFreqVar.ino
@@ -1,35 +1,31 @@
 /*
   Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
+  Pisca segmentos do perimetro de um visor de 7 segmentos em sequencia,
+  com a velocidade ajustada por um potenciometro.
+  
  */
 
 const int PRIM_PINO = 6;
-const int ULT_PINO = 13;
+const int ULT_PINO = 11; // nao piscar ponto nem centro
 const int POTENCIOMETRO = A0;
 const int DELAY = 200;
 int pino_ativo = PRIM_PINO;
 int pot = 0; // valor lido do potenciometro
 
 void setup() {                
-  // initiacializar pinos digitais de saida
+  // inicializar pinos digitais de saida
   for (int pino=PRIM_PINO; pino<=ULT_PINO; pino++) {
     pinMode(pino, OUTPUT);     
   }
-  //Serial.begin(9600); 
 }
 
 void loop() {
   pot = analogRead(POTENCIOMETRO);
-  //Serial.println(pot); 
-  digitalWrite(pino_ativo, HIGH);   // set the LED on
-  delay(pot);              // wait for a second
-  digitalWrite(pino_ativo, LOW);    // set the LED off
-  //delay(DELAY);              // wait for a second
+  digitalWrite(pino_ativo, HIGH);   
+  delay(pot);              
+  digitalWrite(pino_ativo, LOW);  
   pino_ativo++;
-  if (pino_ativo > (ULT_PINO-2)) { // nao piscar ponto nem centro
+  if (pino_ativo > (ULT_PINO)) { 
     pino_ativo = PRIM_PINO;  
-    delay(DELAY);              // wait for a second
   }
 }
--- Blink7Segmentos/Blink7Segmentos.ino
+++ Blink7Segmentos/Blink7Segmentos.ino
@@ -1,8 +1,6 @@
 /*
   Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
+  Pisca todos os segmentos de um visor de 7 segmentos em sequencia, depois o ponto.
  */
 
 const int PRIM_PINO = 6;
@@ -11,21 +9,18 @@ const int DELAY = 200;
 int pino_ativo = PRIM_PINO;
 
 void setup() {                
-  // initialize the digital pin as an output.
-  // Pin 13 has an LED connected on most Arduino boards:
   for (int pino=PRIM_PINO; pino<=ULT_PINO; pino++) {
     pinMode(pino, OUTPUT);     
   }
 }
 
 void loop() {
-  digitalWrite(pino_ativo, HIGH);   // set the LED on
-  delay(DELAY);                     // wait
-  digitalWrite(pino_ativo, LOW);    // set the LED off
-  //delay(DELAY);                   // wait
+  digitalWrite(pino_ativo, HIGH); 
+  delay(DELAY);                     
+  digitalWrite(pino_ativo, LOW);    
   pino_ativo++;
   if (pino_ativo > ULT_PINO) {
     pino_ativo = PRIM_PINO;  
-    delay(DELAY);                   // wait
+    delay(DELAY);                  
   }
 }
